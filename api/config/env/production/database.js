49bf02da973463a2723d7bdbfe9d71261c7c9c81
srimanchaudhuri <srimanchaudhuri@gmail.com>
2023-08-05 15:09:22 +0530

    Added Production

--- api/config/env/production/database.js
+++ api/config/env/production/database.js
@@ -1,19 +1,17 @@
+
 module.exports = ({ env }) => ({
     connection: {
-      client: 'mysql', // Specify the database client as MySQL
-  
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306), // Use the default MySQL port
-        database: env('DATABASE_NAME', 'strapidb'),
-        user: env('DATABASE_USERNAME', 'root'), // Use the appropriate MySQL username
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      client: 'postgres', 
+    connection: {
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapidb'),
+          user: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''),
+          ssl: env.bool("DATABASE_SSL", false) && {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
         },
-      },
-  
-      debug: false,
+        },
+        debug: false,
     },
-  });
-  
+  }); 
